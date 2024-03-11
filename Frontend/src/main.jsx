d7ab50da3d03554abdd052d580019c8e44ac05c5
Yash <yash456k@gmail.com>
2024-03-11 20:23:54 +0530

    add backend URL

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./components/App.jsx";
 import axios from "axios";
 import "./index.css";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://agreeable-jersey-pig.cyclic.app";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
