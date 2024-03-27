aadfa80218d1fd1c178930a0798d5bf9f7b4b3b1
Yash <yash456k@gmail.com>
2024-03-27 14:33:59 +0530

    change origin to netlify

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://yash456k-basic-notes-mern-app.netlify.app",
   })
 );
 
